92ab51f327abd04867436b46e787df37b457b771
mottez <hugomottez@live.fr>
2018-11-27 09:28:45 +0100

    init coinmarketcap

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const config = require('./package.json');
  * Create a new Instance of Slack Bot
  */
 const bot = new SlackBot({
-    token: 'xoxb-487717201747-488056503376-z82o2Q3MCf7WuNGViowsFpEd',
+    token: 'my key',
     name: 'cryptoscan',
 });
 
@@ -24,4 +24,28 @@ bot.on('start', function() {
     bot.postMessageToChannel('ebm', 'meow!', params);
 
 
+});
+
+/* Example in Node.js ES6 using request-promise, concepts should translate to your language of choice */
+
+const rp = require('request-promise');
+const requestOptions = {
+    method: 'GET',
+    uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+    qs: {
+        start: 1,
+        limit: 10,
+        convert: 'EUR'
+    },
+    headers: {
+        'X-CMC_PRO_API_KEY': 'my key'
+    },
+    json: true,
+    gzip: true
+};
+
+rp(requestOptions).then(response => {
+    console.log('API call response:', response);
+}).catch((err) => {
+    console.log('API call error:', err.message);
 });
